437c658b25e9066f643ef6416443547dd8d9122a
mulanimohsin5 <mulanimohsin5@gmail.com>
2022-11-29 22:41:36 +0530

    added custom_user

--- profiles_project/profile_api/models.py
+++ profiles_project/profile_api/models.py
@@ -25,7 +25,7 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """ Represent the 'UserProfile ' from the our system"""
+    """ Represent the 'UserProfile' from the our system"""
 
     email = models.EmailField(max_length=255,unique=True)
     name= models.CharField(max_length=255)
