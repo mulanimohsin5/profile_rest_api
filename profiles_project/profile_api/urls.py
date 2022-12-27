8fe514919f964913e760f2b7cf337c5a665ea65c
mulanimohsin5 <mulanimohsin5@gmail.com>
2022-12-28 00:24:18 +0530

    fixed formatting

--- profiles_project/profile_api/urls.py
+++ profiles_project/profile_api/urls.py
@@ -1,12 +1,12 @@
-from rest_framework.urls import path
 from django.urls import include
-
 from rest_framework.routers import DefaultRouter
+from rest_framework.urls import path
+
 from .views import HelloApiView, HelloViewSet
 
-router=DefaultRouter()
-router.register('hello_viewset',HelloViewSet,basename='hello_viewset')
+router = DefaultRouter()
+router.register('hello_viewset', HelloViewSet, basename='hello_viewset')
 urlpatterns = [
-    path("",include(router.urls)),
+    path("", include(router.urls)),
     path('hello-view/', HelloApiView.as_view())
 ]
